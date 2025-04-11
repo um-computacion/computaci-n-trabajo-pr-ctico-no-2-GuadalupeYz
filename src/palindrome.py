d481a58583964415bb3704e984530281f6db2c1a
GuadalupeYz <mg.yanez@alumno.um.edu.ar>
2025-04-11 18:10:59 -0300

    Implementar limpieza de texto en la función is_palindrome

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,2 +1,9 @@
 def is_palindrome(texto):
-    return texto == texto[::-1]
+    caracteres_validos = "abcdefghijklmnopqrstuvwxyz0123456789"
+    texto_limpio = ""
+
+    for c in texto.lower():
+        if c in caracteres_validos:
+            texto_limpio += c
+
+    return texto_limpio == texto_limpio[::-1]
